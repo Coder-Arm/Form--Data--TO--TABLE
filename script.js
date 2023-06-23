fc1daf808f6d537989ad384a88db3e5dd2c5a9a1
Coder-Arm <anuastro002@gmail.com>
2023-06-23 14:21:16 +0530

    pushing code

---
+++ script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form");
+const tbody = document.getElementsByTagName("tbody")[0];
+let html = "";
+function userTable(user){
+	  html = html+`<tr>
+	                     <td>${user.rollNo}</td>
+	                     <td>${user.name}</td>
+	                     <td>${user.college}</td>
+	                     <td>${user.status}</td>
+	                     </tr>`
+	tbody.innerHTML = html; 
+}
+
+
+
+form.addEventListener("submit",(e) => {
+	e.preventDefault();
+     let user = {
+		  rollNo : form["rollNo"].value,
+		  name : form["firstName"].value+" "+form["lastName"].value,
+		  college : form["collegeName"].value,
+          status : form["status"].value
+	 }
+	//  console.log(user)
+       userTable(user);
+})
